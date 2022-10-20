f81c156b7e834dd0b10f346824edff8cf23f3697
choice-developer <developer@choicehumanitarian.org>
2022-10-20 13:58:02 -0600

    Created GPU class and ability to view GPU attributes

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from GPU import GPU
+
 
 def main():
+
+    rtx_4090 = GPU(name='RTX 4090', model='MSI Dragon Cum', brand='NVIDIA', msrp=1300.00, tdp=400,
+                   release_date='09-15-2022')
     looper = True
     while looper:
         print('Choose from the following:')
-        selection = int(input('\n1 Say Hi\n2 Say Bye\n3 Quit\nSelection: '))
+        selection = int(input('\n1 GPU List\n2 Say Bye\n3 Quit\nSelection: '))
 
         if selection == 1:
-            print('Hi')
+            print(rtx_4090.get_all())
         elif selection == 2:
             print('Bye')
         elif selection == 3:
