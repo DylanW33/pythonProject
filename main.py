1a4238fa7dbcdc5a7274297405df658f90a1e80e
DylanW033 <mrdylanwarren@gmail.com>
2022-10-20 16:25:24 -0600

    I went and wrote CPU info in CPU.py file then imported CPU file into the main and named each thing (ie: name,model, brand etc.). i then edited the selection input list and added CPU to option 2. I then inputted the CPU List into the if else statement and had it print ryzen_5900x.get_all(). And ended it with the looper = false.

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from GPU import GPU
-
+from CPU import CPU
 
 def main():
+
+    ryzen_5900x = CPU(name='Ryzen 9', model='5900x', brand='AMD', msrp=380.00, tdp=105, release_date='11-05-2020')
 
     rtx_4090 = GPU(name='RTX 4090', model='MSI Dragon Cum', brand='NVIDIA', msrp=1300.00, tdp=400,
                    release_date='09-15-2022')
     looper = True
     while looper:
         print('Choose from the following:')
-        selection = int(input('\n1 GPU List\n2 Say Bye\n3 Quit\nSelection: '))
+        selection = int(input('\n1 GPU List\n2 CPU List\n3 Say Bye\n4 Quit\nSelection: '))
 
         if selection == 1:
             print(rtx_4090.get_all())
         elif selection == 2:
-            print('Bye')
+            print(ryzen_5900x.get_all())
         elif selection == 3:
+            print('Say Bye')
+        elif selection == 4:
             looper = False
+
 
 main()
